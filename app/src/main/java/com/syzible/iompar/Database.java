8f996be5681051b67120fe66c2e80d823083bf78
Ed <oflynned@tcd.ie>
2015-10-29 15:02:50 +0000

    added base of DB helpers

--- app/src/main/java/com/syzible/iompar/Database.java
+++ app/src/main/java/com/syzible/iompar/Database.java
@@ -1,7 +1,51 @@
 package com.syzible.iompar;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by ed on 28/10/15.
  */
 public class Database {
+    public static final String DATABASE_NAME = "iompar_db";
+
+    public Database(){}
+
+    //table to allow for regular stops to be accessed quickly
+    public static abstract class Favourites implements BaseColumns {
+        //column names
+        public static final String TABLE_NAME = "favourites_log";
+        //allocate an id to each entity added to the table
+        public static final String STOP_ID = "stop_id"; //int
+        //bus, luas, train...
+        public static final String TRANSIT_TYPE = "transit_type"; //string
+        //ie charlemont (ordered from stephen's green to bride's glen) via luas would be #3
+        //ie st john's ambulance via Dublin Bus would be #907 from the website etc
+        public static final String STOP_NUMBER = "stop"; //int
+    }
+
+    //table to keep a regular transit set as a notification over time if added
+    //ie I get a bus everyday at 8:41am with a variance of x mins, therefore I need to be notified
+    public static abstract class RegularNotifications implements BaseColumns {
+        public static final String TABLE_NAME = "regular_log";
+        public static final String TRANSIT_ID = "transit_id"; //int
+        public static final String TRANSIT_TYPE = "transit_type"; //string, train bus tram...
+        public static final String TIME = "time"; //time of departure; 08:41 ... 18:10 ...
+    }
+
+    //table to access current leapcard balance from spending so you can track
+    public static abstract class LeapBalance implements BaseColumns {
+        public static final String TABLE_NAME = "leap_balance_log";
+        public static final String LEAP_ID = "leap_id"; //id for row
+        public static final String LEAP_NUMBER = "leap_number"; //long, 14 digit long registration number
+        public static final String LEAP_BALANCE = "balance"; //double, balance on leapcard
+    }
+
+    //table to store login information to retrieve actual balance via json
+    public static abstract class LeapLogin implements BaseColumns {
+        public static final String TABLE_NAME = "leap_log_info";
+        public static final String LEAP_ID = "leap_id"; //most likely will only ever be 1
+        public static final String LEAP_NUMBER = "leap_number"; //long, 14 digit long registration number
+        public static final String USER_EMAIL = "email"; //string, for login with leapcard.ie
+        public static final String USER_PASSWORD = "password"; //string, password for login
+    }
 }
--- app/src/main/java/com/syzible/iompar/DatabaseHelper.java
+++ app/src/main/java/com/syzible/iompar/DatabaseHelper.java
@@ -1,7 +1,123 @@
 package com.syzible.iompar;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.Time;
+
 /**
  * Created by ed on 28/10/15.
  */
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    private Context context;
+
+    //queries
+    public static final String q = "";
+
+    public DatabaseHelper(Context context){
+        super(context, Database.DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    //4 tables:
+    //#1: favourites
+    public long insertFavourite(int stopId, int stopNumber, String transitType){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();//create instance; id_col = col 0
+        contentValues.put(Database.Favourites.STOP_ID, stopId); //col 1
+        contentValues.put(Database.Favourites.STOP_NUMBER, stopNumber); //col 2
+        contentValues.put(Database.Favourites.TRANSIT_TYPE, transitType); //col 3
+
+        return sqLiteDatabase.insert(Database.Favourites.TABLE_NAME, null, contentValues);
+    }
+
+    public void removeFavourite(){
+        SQLiteDatabase sqLiteDatabaseReadable = this.getReadableDatabase();
+        SQLiteDatabase sqLiteDatabaseWritable = this.getWritableDatabase();
+
+        String deleteFavouriteFromTable = "DELETE FROM " + Database.Favourites.TABLE_NAME +
+                                            " WHERE " + Database.Favourites.STOP_ID + "=";
+
+        sqLiteDatabaseWritable.execSQL(deleteFavouriteFromTable);
+    }
+
+    public void updateFavourite(){
+
+    }
+
+    //#2: notifications for regular times
+    public long addRegular(int transitId, String transitType, Time time){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();//create instance; id_col = col 0
+        contentValues.put(Database.RegularNotifications.TRANSIT_ID, transitId); //col 1
+        contentValues.put(Database.RegularNotifications.TRANSIT_TYPE, transitType); //col 2
+        contentValues.put(Database.RegularNotifications.TIME, String.valueOf(time)); //col 3
+
+        return sqLiteDatabase.insert(Database.RegularNotifications.TABLE_NAME, null, contentValues);
+    }
+
+    public void removeRegular(){
+
+    }
+
+    public void updateRegular(){
+
+    }
+
+    //#3: recorded leap balance
+    public long addExpenditure(int leapId, long number, double balance){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();//create instance; id_col = col 0
+        contentValues.put(Database.LeapBalance.LEAP_ID, leapId); //col 1
+        contentValues.put(Database.LeapBalance.LEAP_NUMBER, number); //col 2
+        contentValues.put(Database.LeapBalance.LEAP_BALANCE, balance); //col 3
+
+        return sqLiteDatabase.insert(Database.LeapBalance.TABLE_NAME, null, contentValues);
+    }
+
+    public void removeExpenditure(){
+
+    }
+
+    public void updateExpenditure(){
+
+    }
+
+    //#4: information for leap login
+    public long addLeap(int leapId, String number, String email, String password){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();//create instance; id_col = col 0
+        contentValues.put(Database.LeapLogin.LEAP_ID, leapId); //col 1
+        contentValues.put(Database.LeapLogin.LEAP_NUMBER, number); //col 2
+        contentValues.put(Database.LeapLogin.USER_EMAIL, email); //col 3
+        contentValues.put(Database.LeapLogin.USER_PASSWORD, password); //col 4
+
+        return sqLiteDatabase.insert(Database.LeapLogin.TABLE_NAME, null, contentValues);
+
+    }
+
+    public void removeLeap(){
+
+    }
+
+    public void updateLeap(){
+
+    }
 }
