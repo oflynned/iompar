7665489932a2217790c428e528674671f9a54774
Ed <oflynned@tcd.ie>
2015-12-02 21:31:59 +0000

    added proper implementations for generic functions of database modelling and most of the base helper functions for favourites

--- app/src/main/java/com/syzible/iompar/Database.java
+++ app/src/main/java/com/syzible/iompar/Database.java
@@ -11,41 +11,59 @@ public class Database {
     public Database(){}
 
     //table to allow for regular stops to be accessed quickly
-    public static abstract class Favourites implements BaseColumns {
-        //column names
-        public static final String TABLE_NAME = "favourites_log";
-        //allocate an id to each entity added to the table
-        public static final String STOP_ID = "stop_id"; //int
-        //bus, luas, train...
-        public static final String TRANSIT_TYPE = "transit_type"; //string
-        //ie charlemont (ordered from stephen's green to bride's glen) via luas would be #3
-        //ie st john's ambulance via Dublin Bus would be #907 from the website etc
-        public static final String STOP_NUMBER = "stop"; //int
+    public static abstract class DublinBusFavourites implements BaseColumns {
+        public static final String TABLE_NAME = "dublin_bus_favourites";
+        public static final String ID = "id";
+        public static final String STOP_NUMBER = "stop_number";
+        public static final String ROUTE = "route";
+        public static final String FREQUENCY = "frequency";
     }
 
-    //table to keep a regular transit set as a notification over time if added
-    //ie I get a bus everyday at 8:41am with a variance of x mins, therefore I need to be notified
-    public static abstract class RegularNotifications implements BaseColumns {
-        public static final String TABLE_NAME = "regular_log";
-        public static final String TRANSIT_ID = "transit_id"; //int
-        public static final String TRANSIT_TYPE = "transit_type"; //string, train bus tram...
-        public static final String TIME = "time"; //time of departure; 08:41 ... 18:10 ...
+    public static abstract class BusEireannFavourites implements BaseColumns {
+        public static final String TABLE_NAME = "bus_eireann_favourites";
+        public static final String ID = "id";
+        public static final String STOP_NUMBER = "stop_number";
+        public static final String ROUTE = "route";
+        public static final String DESTINATION = "destination";
+        public static final String FREQUENCY = "frequency";
     }
 
-    //table to access current leapcard balance from spending so you can track
+    public static abstract class LuasFavourites implements BaseColumns {
+        public static final String TABLE_NAME = "luas_favourites";
+        public static final String ID = "id";
+        public static final String STATION = "station";
+        public static final String DIRECTION = "direction";
+        public static final String FREQUENCY = "frequency";
+    }
+
+    public static abstract class DartFavourites implements BaseColumns {
+        public static final String TABLE_NAME = "dart_favourites";
+        public static final String ID = "id";
+        public static final String STATION = "station";
+        public static final String DIRECTION = "direction";
+        public static final String FREQUENCY = "frequency";
+    }
+
+    public static abstract class TrainFavourites implements BaseColumns {
+        public static final String TABLE_NAME = "train_favourites";
+        public static final String ID = "id";
+        public static final String STATION = "station";
+        public static final String DIRECTION = "direction";
+        public static final String FREQUENCY = "frequency";
+    }
+
     public static abstract class LeapBalance implements BaseColumns {
-        public static final String TABLE_NAME = "leap_balance_log";
-        public static final String LEAP_ID = "leap_id"; //id for row
-        public static final String LEAP_NUMBER = "leap_number"; //long, 14 digit long registration number
-        public static final String LEAP_BALANCE = "balance"; //double, balance on leapcard
+        public static final String TABLE_NAME = "leap_balance";
+        public static final String ID = "id";
+        public static final String NUMBER = "number";
+        public static final String BALANCE = "balance";
     }
 
-    //table to store login information to retrieve actual balance via json
     public static abstract class LeapLogin implements BaseColumns {
-        public static final String TABLE_NAME = "leap_log_info";
-        public static final String LEAP_ID = "leap_id"; //most likely will only ever be 1
-        public static final String LEAP_NUMBER = "leap_number"; //long, 14 digit long registration number
-        public static final String USER_EMAIL = "email"; //string, for login with leapcard.ie
-        public static final String USER_PASSWORD = "password"; //string, password for login
+        public static final String TABLE_NAME = "leap_login_info";
+        public static final String ID = "id";
+        public static final String CARD_NUMBER = "number";
+        public static final String USER_EMAIL = "email";
+        public static final String USER_PASSWORD = "password";
     }
 }
--- app/src/main/java/com/syzible/iompar/DatabaseHelper.java
+++ app/src/main/java/com/syzible/iompar/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.syzible.iompar;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -15,110 +16,199 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     private Context context;
 
-    //queries
-    public static final String q = "";
+    //QUERIES
+    //favourites
+    public static final String CREATE_TABLE_DUBLIN_BUS_FAVOURITES =
+            "CREATE TABLE " +
+                    Database.DublinBusFavourites.TABLE_NAME + "(" +
+                    Database.DublinBusFavourites.ID + " INTEGER PRIMARY KEY," +
+                    Database.DublinBusFavourites.STOP_NUMBER + " INTEGER," +
+                    Database.DublinBusFavourites.ROUTE + " TEXT," +
+                    Database.DublinBusFavourites.FREQUENCY + " INTEGER;";
+
+    public static final String CREATE_TABLE_BUS_EIREANN_FAVOURITES =
+            "CREATE TABLE " +
+                    Database.BusEireannFavourites.TABLE_NAME + "(" +
+                    Database.BusEireannFavourites.ID + " INTEGER PRIMARY KEY," +
+                    Database.BusEireannFavourites.STOP_NUMBER + " INTEGER," +
+                    Database.BusEireannFavourites.ROUTE + " TEXT," +
+                    Database.BusEireannFavourites.DESTINATION + " TEXT," +
+                    Database.BusEireannFavourites.FREQUENCY + " INTEGER;";
+
+    public static final String CREATE_TABLE_LUAS_FAVOURITES =
+            "CREATE TABLE " +
+                    Database.LuasFavourites.TABLE_NAME + "(" +
+                    Database.LuasFavourites.ID + " INTEGER PRIMARY KEY," +
+                    Database.LuasFavourites.STATION + " TEXT," +
+                    Database.LuasFavourites.DIRECTION + " TEXT," +
+                    Database.LuasFavourites.FREQUENCY + " INTEGER;";
+
+    public static final String CREATE_TABLE_DART_FAVOURITES =
+            "CREATE TABLE " +
+                    Database.DartFavourites.TABLE_NAME + "(" +
+                    Database.DartFavourites.ID + " INTEGER PRIMARY KEY," +
+                    Database.DartFavourites.STATION + " TEXT," +
+                    Database.DartFavourites.DIRECTION + " TEXT," +
+                    Database.DartFavourites.FREQUENCY + " INTEGER;";
+
+    public static final String CREATE_TABLE_TRAIN_FAVOURITES =
+            "CREATE TABLE " +
+                    Database.TrainFavourites.TABLE_NAME + "(" +
+                    Database.TrainFavourites.ID + " INTEGER PRIMARY KEY," +
+                    Database.TrainFavourites.STATION + " TEXT," +
+                    Database.TrainFavourites.DIRECTION + " TEXT," +
+                    Database.TrainFavourites.FREQUENCY + " INTEGER;";
+
+    public static final String DELETE_TABLE_DUBLIN_BUS =
+            "DROP TABLE IF EXISTS " + Database.DublinBusFavourites.TABLE_NAME + ";";
+
+    public static final String DELETE_TABLE_BUS_EIREANN =
+            "DROP TABLE IF EXISTS " + Database.BusEireannFavourites.TABLE_NAME + ";";
+
+    public static final String DELETE_TABLE_LUAS =
+            "DROP TABLE IF EXISTS " + Database.LuasFavourites.TABLE_NAME + ";";
+
+    public static final String DELETE_TABLE_DART =
+            "DROP TABLE IF EXISTS " + Database.DartFavourites.TABLE_NAME + ";";
+
+    public static final String DELETE_TABLE_TRAIN =
+            "DROP TABLE IF EXISTS " + Database.TrainFavourites.TABLE_NAME + ";";
+
+    public static final String SELECT_ALL_DUBLIN_BUS =
+            "SELECT * FROM " + Database.DublinBusFavourites.TABLE_NAME + ";";
+
+    public static final String SELECT_ALL_BUS_EIREANN =
+            "SELECT * FROM " + Database.BusEireannFavourites.TABLE_NAME + ";";
+
+    public static final String SELECT_ALL_LUAS =
+            "SELECT * FROM " + Database.LuasFavourites.TABLE_NAME + ";";
+
+    public static final String SELECT_ALL_DART =
+            "SELECT * FROM " + Database.DartFavourites.TABLE_NAME + ";";
+
+    public static final String SELECT_ALL_TRAIN =
+            "SELECT * FROM " + Database.TrainFavourites.TABLE_NAME + ";";
 
     public DatabaseHelper(Context context){
         super(context, Database.DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE_BUS_EIREANN_FAVOURITES);
+        db.execSQL(CREATE_TABLE_DART_FAVOURITES);
+        db.execSQL(CREATE_TABLE_DUBLIN_BUS_FAVOURITES);
+        db.execSQL(CREATE_TABLE_LUAS_FAVOURITES);
+        db.execSQL(CREATE_TABLE_TRAIN_FAVOURITES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DELETE_TABLE_BUS_EIREANN);
+        db.execSQL(DELETE_TABLE_DART);
+        db.execSQL(DELETE_TABLE_DUBLIN_BUS);
+        db.execSQL(DELETE_TABLE_LUAS);
+        db.execSQL(DELETE_TABLE_TRAIN);
+        onCreate(db);
+    }
+
+    public void insertRecord(String tableName,
+                           String stopNumber, String station,
+                           String route, String direction,
+                           String destination, int frequency){
+        SQLiteDatabase writeDb = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        switch (tableName){
+            case Database.BusEireannFavourites.TABLE_NAME:
+                contentValues.put(Database.BusEireannFavourites.STOP_NUMBER, stopNumber);
+                contentValues.put(Database.BusEireannFavourites.ROUTE, route);
+                contentValues.put(Database.BusEireannFavourites.DESTINATION, destination);
+                contentValues.put(Database.BusEireannFavourites.FREQUENCY, frequency);
+                break;
+            case Database.DartFavourites.TABLE_NAME:
+                contentValues.put(Database.DartFavourites.STATION, station);
+                contentValues.put(Database.DartFavourites.DIRECTION, direction);
+                contentValues.put(Database.DartFavourites.FREQUENCY, frequency);
+                break;
+            case Database.DublinBusFavourites.TABLE_NAME:
+                contentValues.put(Database.DublinBusFavourites.STOP_NUMBER, stopNumber);
+                contentValues.put(Database.DublinBusFavourites.ROUTE, route);
+                contentValues.put(Database.DublinBusFavourites.FREQUENCY, frequency);
+                break;
+            case Database.LuasFavourites.TABLE_NAME:
+                contentValues.put(Database.LuasFavourites.STATION, station);
+                contentValues.put(Database.LuasFavourites.DIRECTION, direction);
+                contentValues.put(Database.LuasFavourites.FREQUENCY, frequency);
+                break;
+            case Database.TrainFavourites.TABLE_NAME:
+                contentValues.put(Database.LuasFavourites.STATION, station);
+                contentValues.put(Database.LuasFavourites.DIRECTION, direction);
+                contentValues.put(Database.LuasFavourites.FREQUENCY, frequency);
+                break;
+        }
+
+        writeDb.close();
+    }
+
+    public void removeRecord(String tableName, int id){
+        SQLiteDatabase writeDb = this.getWritableDatabase();
+
+        String removeRowQuery =
+                "DELETE FROM " + tableName +
+                        " WHERE " + tableName + ".ID = " + id + ";";
+        writeDb.execSQL(removeRowQuery);
+        writeDb.close();
+    }
+
+    public void modifyRecord(String tableName,
+                             String stopNumber, String station,
+                             String route, String direction,
+                             String destination){
 
     }
 
-    //4 tables:
-    //#1: favourites
-    public long insertFavourite(int stopId, int stopNumber, String transitType){
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+    public void modifyFrequency(String tableName, int id, int frequency){
+        SQLiteDatabase writeDb = this.getWritableDatabase();
 
-        ContentValues contentValues = new ContentValues();//create instance; id_col = col 0
-        contentValues.put(Database.Favourites.STOP_ID, stopId); //col 1
-        contentValues.put(Database.Favourites.STOP_NUMBER, stopNumber); //col 2
-        contentValues.put(Database.Favourites.TRANSIT_TYPE, transitType); //col 3
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(tableName + ".ID", frequency);
 
-        return sqLiteDatabase.insert(Database.Favourites.TABLE_NAME, null, contentValues);
+        String[] whereArgs = {String.valueOf(id)};
+        writeDb.update(tableName, contentValues,
+                tableName + ".ID" + "=" + "?", whereArgs);
+        writeDb.close();
     }
 
-    public void removeFavourite(int stopId){
-        SQLiteDatabase sqLiteDatabaseReadable = this.getReadableDatabase();
-        SQLiteDatabase sqLiteDatabaseWritable = this.getWritableDatabase();
-
-        String deleteFavouriteFromTable = "DELETE FROM " + Database.Favourites.TABLE_NAME +
-                                            " WHERE " + Database.Favourites.STOP_ID +
-                                            "=" + stopId;
-
-        sqLiteDatabaseWritable.execSQL(deleteFavouriteFromTable);
+    public void clearTable(String tableName){
+        SQLiteDatabase writeDb = this.getWritableDatabase();
+        String clearTableQuery =
+                "DELETE FROM " + tableName + ";";
+        writeDb.execSQL(clearTableQuery);
+        writeDb.close();
     }
 
-    public void updateFavourite(){
+    public void printTableContents(String tableName){
+        SQLiteDatabase readDb = this.getReadableDatabase();
+        Cursor cursor = readDb.rawQuery("SELECT * FROM " + tableName, null);
+        cursor.moveToFirst();
 
+        int rowNum = cursor.getCount();
+        int colNum = cursor.getColumnCount();
+
+        System.out.println("# of rows in " + tableName + " is " + rowNum);
+        System.out.println("# of columns in " + tableName + " is " + colNum);
+
+        for (int r = 0; r < rowNum; r++){
+            for(int c = 0; c < colNum; c++){
+                System.out.println(c + ". " + cursor.getString(c) + "; ");
+            }
+            System.out.println("\n------------");
+            cursor.moveToNext();
+        }
+        cursor.close();
+        readDb.close();
     }
 
-    //#2: notifications for regular times
-    public long addRegular(int transitId, String transitType, Time time){
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();//create instance; id_col = col 0
-        contentValues.put(Database.RegularNotifications.TRANSIT_ID, transitId); //col 1
-        contentValues.put(Database.RegularNotifications.TRANSIT_TYPE, transitType); //col 2
-        contentValues.put(Database.RegularNotifications.TIME, String.valueOf(time)); //col 3
-
-        return sqLiteDatabase.insert(Database.RegularNotifications.TABLE_NAME, null, contentValues);
-    }
-
-    public void removeRegular(){
-
-    }
-
-    public void updateRegular(){
-
-    }
-
-    //#3: recorded leap balance
-    public long addExpenditure(int leapId, long number, double balance){
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();//create instance; id_col = col 0
-        contentValues.put(Database.LeapBalance.LEAP_ID, leapId); //col 1
-        contentValues.put(Database.LeapBalance.LEAP_NUMBER, number); //col 2
-        contentValues.put(Database.LeapBalance.LEAP_BALANCE, balance); //col 3
-
-        return sqLiteDatabase.insert(Database.LeapBalance.TABLE_NAME, null, contentValues);
-    }
-
-    public void removeExpenditure(){
-
-    }
-
-    public void updateExpenditure(){
-
-    }
-
-    //#4: information for leap login
-    public long addLeap(int leapId, String number, String email, String password){
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();//create instance; id_col = col 0
-        contentValues.put(Database.LeapLogin.LEAP_ID, leapId); //col 1
-        contentValues.put(Database.LeapLogin.LEAP_NUMBER, number); //col 2
-        contentValues.put(Database.LeapLogin.USER_EMAIL, email); //col 3
-        contentValues.put(Database.LeapLogin.USER_PASSWORD, password); //col 4
-
-        return sqLiteDatabase.insert(Database.LeapLogin.TABLE_NAME, null, contentValues);
-
-    }
-
-    public void removeLeap(){
-
-    }
-
-    public void updateLeap(){
-
-    }
 }
